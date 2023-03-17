7dd184a3db3824d9f2546ea2a8db95410d510bc6
begonufer <begonufernandez@gmail.com>
2023-03-17 02:02:48 +0000

    brand adjust pad

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
 		<>
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Start Bootstrap</a>
+    <a className="navbar-brand ps-5" href="#">Start Bootstrap</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
